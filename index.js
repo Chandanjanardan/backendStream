7223fb4917ac7b2678991f9b1a8c3406bca1d2b6
Chandan Janardan <chandanjanardan@gmail.com>
2023-07-05 12:31:24 +0530

    adding server

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express")
+
+const app = express()
+
+const PORT= 3300
+
+
+
+
+
+
+
+
+
+
+// 
+app.listen(PORT,(req,res)=>{
+    console.log(`Server running on port ${PORT}...`)
+})
